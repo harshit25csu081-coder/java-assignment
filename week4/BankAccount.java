c3103bf8fd6a881ea19d8a6233e948808dc67e59
harshit25csu081-coder <harshit25csu081@ncuindia.edu>
2026-08-26 23:32:54 +0530

    Create BankAccount.java

---
+++ week4/BankAccount.java
@@ -0,0 +1,48 @@
+public class Account {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+    Account(String accountNumber,String ownerName){
+        this.accountNumber=accountNumber;
+        this.ownerName=ownerName;
+        this.balance=0;
+    }
+    Account(String accountNumber,String ownerName,double balance){
+        this.accountNumber=accountNumber;
+        this.ownerName=ownerName;
+        if (balance>=0){
+            this.balance=balance;
+        }
+            else{
+                this.balance=0;
+            }
+        }
+    boolean deposite(double amount){
+        if(amount>0){
+            balance=balance+amount;
+            return true;
+        }
+        return false;
+    }
+    boolean withdraw(double amount){
+        if(amount>0 && amount<=balance){
+            balance=balance-amount;
+            return true;
+        }
+        return false;
+    }
+String getAccountNumber(){
+     return accountNumber;
+
+}
+    String getOwnerName(){
+        return ownerName;
+    }
+    double getbalance(){
+        return balance;
+    }
+
+public String toString(){
+return "Account number:"+accountNumber+"owner:"+ownerName+"Balance"+balance;
+}
+}
