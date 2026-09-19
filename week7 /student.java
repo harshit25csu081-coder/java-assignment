0420caf754a4342a52b6ba28d132a47ce46288f4
harshit25csu081-coder <harshit25csu081@ncuindia.edu>
2026-09-19 17:06:15 +0530

    Add Student class with validation for marks

---
+++ week7 /student.java
@@ -0,0 +1,28 @@
+package college;
+
+import college.model.InvalidStudentDataException;
+
+public class Student {
+
+    private String name;
+    private int mark;
+
+    public Student(String name) {
+        this.name = TextUtils.normalize(name);
+    }
+
+    public void setMark(int mark) throws InvalidStudentDataException {
+        if (mark < 0 || mark > 100) {
+            throw new InvalidStudentDataException(
+                "Mark must be between 0 and 100."
+            );
+        }
+
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', mark=" + mark + "}";
+    }
+}
